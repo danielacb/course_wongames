2d848753b654f6a8be3c3f971136487712701b4a
Daniela Barbosa <contato@danielacb.com>
2021-08-27 22:34:38 -0300

    create first test on Logo component

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from '../../utils/tests/helpers'
+import theme from 'styles/theme'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: theme.colors.white
+    })
+  })
+
+  it('should render a black label by default', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: theme.colors.black
+    })
+  })
+})

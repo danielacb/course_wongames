61f1d3fb1cad2ce65604733515f103d2622483e5
Daniela Barbosa <contato@danielacb.com>
2021-08-31 23:00:46 -0300

    create Button component

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,36 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import theme from 'styles/theme'
+
+import Button from '.'
+
+describe('<Button />', () => {
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Buy now!</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now!/i })).toHaveStyle({
+      height: '4rem',
+      'font-size': theme.font.sizes.small,
+      padding: `${theme.spacings.xxsmall} ${theme.spacings.medium}`
+    })
+  })
+
+  it('should render the small size', () => {
+    renderWithTheme(<Button size="small">Buy now!</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now!/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': theme.font.sizes.xsmall
+    })
+  })
+
+  it('should render the large size', () => {
+    renderWithTheme(<Button size="large">Buy now!</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy now!/i })).toHaveStyle({
+      height: '5rem',
+      'font-size': theme.font.sizes.medium,
+      padding: `${theme.spacings.xxsmall} ${theme.spacings.xlarge}`
+    })
+  })
+})

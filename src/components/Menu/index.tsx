a75c3090915d594f606c8e6dfb705f397ca3f339
Daniela Barbosa <contato@danielacb.com>
2021-09-02 23:46:23 -0300

    create mobile version of the Menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+import Logo from 'components/Logo'
+
+import * as S from './styles'
+
+const Menu = () => {
+  const [isOpen, setisOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper>
+        <MenuIcon aria-label="Open Menu" onClick={() => setisOpen(true)} />
+      </S.IconWrapper>
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setisOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
+
+export default Menu
---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,50 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    padding: ${theme.spacings.small} 0;
+    position: relative;
+  `}
+`
+
+export const LogoWrapper = styled.div`
+  ${media.lessThan('medium')`
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  `}
+`
+
+export const IconWrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    cursor: pointer;
+    width: 2.4rem;
+    height: 2.4rem;
+  `}
+`
+
+export const MenuGroup = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+
+    > div {
+      margin-left: ${theme.spacings.xsmall};
+    }
+  `}
+`
+
+type MenuFullProps = {
+  isOpen: boolean
+}
+
+export const MenuFull = styled.nav<MenuFullProps>`
+  ${({ isOpen }) => css`
+    opacity: ${isOpen ? 1 : 0};
+  `}
+`

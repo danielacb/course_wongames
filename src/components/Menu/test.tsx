15560cd3f23a72e3b30ec9707cbe6f1b3e0d8694
Daniela Barbosa <contato@danielacb.com>
2021-09-04 20:57:22 -0300

    add username prop to the Menu component

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -29,4 +29,24 @@ describe('<Menu />', () => {
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
+
+  it('should display register box when not logged in', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+  })
+
+  it('should display my account and wishlist when logged in', () => {
+    renderWithTheme(<Menu username="user" />)
+
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+  })
 })

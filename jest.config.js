b99d81ff2d0c33e275604be7a726f5b0526166dc
Daniela Barbosa <contato@danielacb.com>
2021-09-19 23:13:52 -0300

    add tests and match-media-mock to Slider

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }

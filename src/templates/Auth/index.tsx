32ebe694410e2244bd1a003770ca563eb2be4f9d
Daniela Barbosa <contato@danielacb.com>
2021-10-26 23:23:07 -0300

    create Auth template

---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,40 @@
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+
+import * as S from './styles'
+
+type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
+  <S.Wrapper>
+    <S.BannerBlock>
+      <S.BannerContent>
+        <Logo />
+
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.SubTitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.SubTitle>
+        </div>
+
+        <S.BannerFooter>Won Games 2020 © All Rights Reserved</S.BannerFooter>
+      </S.BannerContent>
+    </S.BannerBlock>
+
+    <S.ContentWrapper>
+      <S.Content>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+        {children}
+      </S.Content>
+    </S.ContentWrapper>
+  </S.Wrapper>
+)
+
+export default Auth
---
+++ src/templates/Auth/styles.ts
@@ -0,0 +1,87 @@
+import styled, { css } from 'styled-components'
+
+import * as HeadingStyles from 'components/Heading/styles'
+import * as LogoStyles from 'components/Logo/styles'
+
+export const Wrapper = styled.main`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 100vh;
+`
+
+export const BannerBlock = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    background-image: url('/img/auth-bg.jpg');
+    background-size: cover;
+    background-position: center center;
+    padding: ${theme.spacings.large};
+
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background-color: ${theme.colors.black};
+      opacity: 0.85;
+    }
+  `}
+`
+
+export const BannerContent = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    position: relative;
+    z-index: ${theme.layers.base};
+
+    display: grid;
+    grid-template-columns: 1fr;
+    height: 100%;
+    justify-content: space-between;
+  `}
+`
+
+export const SubTitle = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xxlarge};
+    font-weight: ${theme.font.light};
+    margin-top: ${theme.spacings.xxsmall};
+
+    strong {
+      color: ${theme.colors.primary};
+    }
+  `}
+`
+
+export const BannerFooter = styled.p`
+  ${({ theme }) => css`
+    align-self: end;
+    font-size: ${theme.font.sizes.xsmall};
+    text-align: center;
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    width: 30rem;
+
+    ${HeadingStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+
+    ${LogoStyles.Wrapper} {
+      margin: 0 auto ${theme.spacings.xxlarge};
+    }
+  `}
+`
+
+export const ContentWrapper = styled.div`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.white};
+    display: grid;
+    align-items: center;
+    justify-content: center;
+  `}
+`

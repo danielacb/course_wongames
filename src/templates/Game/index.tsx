b98ec0df8e8d74e71c1a3424540a5440cde191e3
Daniela Barbosa <contato@danielacb.com>
2021-11-20 20:10:12 -0300

    add static gallery to Game template

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,20 +1,28 @@
 import Base from 'templates/Base'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
+import Gallery, { GalleryImageProps } from 'components/Gallery'
 
 import * as S from './styles'
 
 export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
+  gallery?: GalleryImageProps[]
 }
 
-const Game = ({ cover, gameInfo }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
     <S.Main>
       <S.SectionGameInfo>
         <GameInfo {...gameInfo} />
       </S.SectionGameInfo>
+
+      {!!gallery && (
+        <S.SectionGallery>
+          <Gallery images={gallery} />
+        </S.SectionGallery>
+      )}
     </S.Main>
   </Base>
 )

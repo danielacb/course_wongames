e91d5b07d0ff8d8d87e6ced4cdf24f7cfc324b7b
Daniela Barbosa <contato@danielacb.com>
2021-11-21 19:50:04 -0300

    add static TextContent to Game template

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,6 +1,7 @@
 import Base from 'templates/Base'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
+import TextContent from 'components/TextContent'
 
 import * as S from './styles'
 
@@ -8,9 +9,10 @@ export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
+  description: string
 }
 
-const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
     <S.Main>
@@ -23,6 +25,10 @@ const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
           <Gallery images={gallery} />
         </S.SectionGallery>
       )}
+
+      <S.SectionDescription>
+        <TextContent title="Description" content={description} />
+      </S.SectionDescription>
     </S.Main>
   </Base>
 )

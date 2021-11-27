183da017a406729ee1f5175c45e1857d34f681e5
Daniela Barbosa <contato@danielacb.com>
2021-11-26 22:49:28 -0300

    add static Showcase to Wishlist template

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,14 +1,32 @@
 import Base from 'templates/Base'
-import { Container } from 'components/Container'
-import Heading from 'components/Heading'
 
-const Wishlist = () => (
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+
+export type WishlistProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
   </Base>
 )
 

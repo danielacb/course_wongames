ff9992f60be14930daed9bc9e35d5a5fa11dcc30
Daniela Barbosa <contato@danielacb.com>
2021-11-28 22:01:06 -0300

    create Grid component

--- src/components/Container/test.tsx
+++ src/components/Container/test.tsx
@@ -18,6 +18,7 @@ describe('<Container />', () => {
 
     expect(container.firstChild).toMatchInlineSnapshot(`
       .c0 {
+        width: 100%;
         margin-left: auto;
         margin-right: auto;
         max-width: 130rem;

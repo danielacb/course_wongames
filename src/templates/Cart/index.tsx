d2256583ad4f6b22072e6e7c33920056b3852056
Daniela Barbosa <contato@danielacb.com>
2022-01-24 22:20:20 -0300

    create Cart page

---
+++ src/templates/Cart/index.tsx
@@ -0,0 +1,61 @@
+import { Container } from 'components/Container'
+import { Divider } from 'components/Divider'
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import EmptyState from 'components/EmptyState'
+import Base from 'templates/Base'
+
+import { GameCardProps } from 'components/GameCard'
+import PaymentOptions, { PaymentOptionsProps } from 'components/PaymentOptions'
+import { HighlightProps } from 'components/Highlight'
+import CartList, { CartListProps } from 'components/CartList'
+
+import * as S from './styles'
+
+export type CartProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+} & CartListProps &
+  Pick<PaymentOptionsProps, 'cards'>
+
+const Cart = ({
+  recommendedGames,
+  recommendedHighlight,
+  items,
+  total,
+  cards
+}: CartProps) => {
+  const handlePayment = () => ({})
+
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My Cart
+        </Heading>
+
+        {items.length ? (
+          <S.Content>
+            <CartList items={items} total={total} />
+            <PaymentOptions cards={cards} handlePayment={handlePayment} />
+          </S.Content>
+        ) : (
+          <EmptyState
+            title="Your cart is empty"
+            description="Go back to the store and explore great games and offers"
+            hasLink
+          />
+        )}
+
+        <Divider />
+      </Container>
+      <Showcase
+        title="You may like these games"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
+
+export default Cart

3769e0eee6e0590430440a00bd446af5e6e3b539
Daniela Barbosa <contato@danielacb.com>
2022-02-23 22:48:21 -0300

    setup Apollo Client and Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,35 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="Wongames - Game e-commerce using TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="Wongames - Game e-commerce using TypeScript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 

df91588c8ecc95e6bf6e94920a0d53e6dd357ac2
Daniela Barbosa <contato@danielacb.com>
2022-03-08 23:24:35 -0300

    add dynamic upcoming and free games sections to the Home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -15,7 +15,6 @@ export type HomeTemplateProps = {
   mostPopularGames: GameCardProps[]
   upcomingGames: GameCardProps[]
   upcomingHighlight: HighlightProps
-  upcomingMoreGames: GameCardProps[]
   freeGames: GameCardProps[]
   freeHighlight: HighlightProps
 }
@@ -27,7 +26,6 @@ const Home = ({
   mostPopularGames,
   upcomingGames,
   upcomingHighlight,
-  upcomingMoreGames,
   freeGames,
   freeHighlight
 }: HomeTemplateProps) => {
@@ -49,10 +47,11 @@ const Home = ({
         games={mostPopularGames}
       />
 
-      <S.SectionUpcoming>
-        <Showcase title="Upcoming" games={upcomingGames} />
-        <Showcase highlight={upcomingHighlight} games={upcomingMoreGames} />
-      </S.SectionUpcoming>
+      <Showcase
+        title="Upcoming"
+        games={upcomingGames}
+        highlight={upcomingHighlight}
+      />
 
       <Showcase
         title="Free games"
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -15,7 +15,6 @@ const props = {
   mostPopularGames: [gamesMock[0]],
   upcomingGames: [gamesMock[0]],
   upcomingHighlight: highlightMock,
-  upcomingMoreGames: [gamesMock[0]],
   freeGames: [gamesMock[0]],
   freeHighlight: highlightMock
 }
@@ -43,6 +42,5 @@ describe('<Home />', () => {
     renderWithTheme(<Home {...props} />)
 
     expect(screen.getByTestId('Mock Banner Slider')).toBeInTheDocument()
-    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5)
   })
 })

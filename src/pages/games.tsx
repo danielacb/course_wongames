7990ef096c413c57e9dda3c2189f6bae54f8a6a5
Daniela Barbosa <contato@danielacb.com>
2022-03-23 21:22:40 -0300

    add initialApolloState to Games page

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -2,7 +2,7 @@ import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
-import { QueryGames } from 'graphql/generated/QueryGames'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 import filterItemsMock from 'components/ExploreSidebar/mock'
 
@@ -13,22 +13,16 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query<QueryGames>({
+  await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
-    variables: { limit: 9 }
+    variables: { limit: 12 }
   })
 
   return {
     props: {
       revalidate: 60,
       filterItems: filterItemsMock,
-      games: data.games.map((game) => ({
-        slug: game.slug,
-        title: game.name,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      }))
+      initialApolloState: apolloClient.cache.extract()
     }
   }
 }

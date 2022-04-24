bc92c2c4c121e51c815e599f764487f4a93d1160
Daniela Barbosa <contato@danielacb.com>
2022-04-23 21:08:24 -0300

    remove a few files from collectCoverage

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,11 @@ module.exports = {
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
     '!src/utils/apollo.ts',
-    '!src/types/**/*.d.ts'
+    '!src/types/**/*.d.ts',
+    '!src/utils/apolloCache.ts',
+    '!src/types/**/*.d.ts',
+    '!src/graphql/**/*.ts',
+    '!src/**/mock.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleDirectories: ['node_modules', 'src'],

2aa5f74ff39c7c3c369ed8d7484396b029f5d2b9
Daniela Barbosa <contato@danielacb.com>
2022-05-24 22:10:33 -0300

    add CartProvidert to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
+import { ApolloProvider } from '@apollo/client'
+import { CartProvider } from 'hooks/use-cart'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-
-import { ApolloProvider } from '@apollo/client'
-import { useApollo } from 'utils/apollo'
-
 import { ThemeProvider } from 'styled-components'
-import theme from 'styles/theme'
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+import { useApollo } from 'utils/apollo'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
@@ -14,18 +13,20 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="Wongames - Game e-commerce using TypeScript, React, NextJS and Styled Components"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <CartProvider>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="Wongames - Game e-commerce using TypeScript, React, NextJS and Styled Components"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </CartProvider>
       </ThemeProvider>
     </ApolloProvider>
   )

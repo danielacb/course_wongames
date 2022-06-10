124ef2e015e4cba213c84f605f2f329887a39fd8
Daniela Barbosa <contato@danielacb.com>
2022-06-09 22:30:15 -0300

    add NextNprogress loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from '@apollo/client'
 import { CartProvider } from 'hooks/use-cart'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import NextNprogress from 'nextjs-progressbar'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
@@ -25,6 +26,14 @@ function App({ Component, pageProps }: AppProps) {
             />
           </Head>
           <GlobalStyles />
+
+          <NextNprogress
+            color="#F231A5"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={3}
+          />
+
           <Component {...pageProps} />
         </CartProvider>
       </ThemeProvider>

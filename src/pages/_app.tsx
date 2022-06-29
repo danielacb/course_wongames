c4c613a7faaccd90b74e3faab93e67c0795f7556
Daniela Barbosa <contato@danielacb.com>
2022-06-28 23:22:57 -0300

    NextAuth initial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client'
 import { CartProvider } from 'hooks/use-cart'
+import { Provider as AuthProvider } from 'next-auth/client'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import NextNprogress from 'nextjs-progressbar'
@@ -12,32 +13,34 @@ function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="Wongames - Game e-commerce using TypeScript, React, NextJS and Styled Components"
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="Wongames - Game e-commerce using TypeScript, React, NextJS and Styled Components"
+              />
+            </Head>
+            <GlobalStyles />
+
+            <NextNprogress
+              color="#F231A5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
             />
-          </Head>
-          <GlobalStyles />
 
-          <NextNprogress
-            color="#F231A5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={3}
-          />
-
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
 

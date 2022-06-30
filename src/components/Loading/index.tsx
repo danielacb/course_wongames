52dca8eb8c2e4d3590ef9516c142794afd6acf16
Daniela Barbosa <contato@danielacb.com>
2022-06-29 21:14:30 -0300

    add size prop to Loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,12 +2,15 @@ import styled, { css, DefaultTheme, keyframes } from 'styled-components'
 
 export type LoadingProps = {
   color?: 'primary' | 'secondary' | 'white'
+  size?: 'small' | 'medium' | 'large'
 }
 
 const Wrapper = styled.div`
   width: 100%;
   text-align: center;
-  margin: 1rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const spin = keyframes`
@@ -34,27 +37,43 @@ const loadingModifier = {
   white: (theme: DefaultTheme) => css`
     border-top-color: ${theme.colors.white};
     border-right-color: ${theme.colors.white};
+  `,
+
+  small: () => css`
+    width: 2rem;
+    height: 2rem;
+    border-width: 0.3rem;
+  `,
+
+  medium: () => css`
+    width: 4rem;
+    height: 4rem;
+    border-width: 0.5rem;
+  `,
+
+  large: () => css`
+    width: 6rem;
+    height: 6rem;
+    border-width: 0.7rem;
   `
 }
 
 const Spinner = styled.div<LoadingProps>`
-  ${({ theme, color = 'primary' }) => css`
+  ${({ theme, color = 'primary', size = 'medium' }) => css`
     display: inline-block;
     border-color: rgba(255, 255, 255, 0.2);
     border-style: solid;
-    border-width: 0.5rem;
     border-radius: 50%;
-    width: 4rem;
-    height: 4rem;
     animation: ${spin} 1s infinite linear;
 
+    ${size && loadingModifier[size]}
     ${color && loadingModifier[color](theme)}
   `}
 `
 
-const Loading = ({ color }: LoadingProps) => (
+const Loading = ({ color, size }: LoadingProps) => (
   <Wrapper data-testid="loading">
-    <Spinner color={color} />
+    <Spinner color={color} size={size} />
   </Wrapper>
 )
 

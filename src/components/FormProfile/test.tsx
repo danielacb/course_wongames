a5ff6bad9ed28ada2713466686f0c17cc52a8f96
Daniela Barbosa <contato@danielacb.com>
2022-08-06 22:43:21 -0300

    update FormProfile tests

--- src/components/FormProfile/test.tsx
+++ src/components/FormProfile/test.tsx
@@ -12,11 +12,8 @@ describe('<FormProfile />', () => {
     expect(screen.getByRole('textbox', { name: /name/i })).toBeInTheDocument()
     expect(screen.getByRole('textbox', { name: /e-mail/i })).toBeInTheDocument()
 
-    expect(
-      screen.getByPlaceholderText(/type your password/i)
-    ).toBeInTheDocument()
-
-    expect(screen.getByPlaceholderText(/new password/i)).toBeInTheDocument()
+    // expect(screen.getByPlaceholderText(/reset password/i)).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /reset password/i }))
 
     expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument()
   })

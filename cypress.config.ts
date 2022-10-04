b9fc5a77cdec307bc3222ed57f26c7ca6b5e062c
Daniela Barbosa <contato@danielacb.com>
2022-10-04 19:28:59 -0300

    refactor cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  video: false,
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack'
+    },
+    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}'
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
+    baseUrl: 'http://localhost:3000'
   }
 })

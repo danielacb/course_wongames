c2c6418e050ffe0cf1d6a20a70e1d57b730b176f
Daniela Barbosa <contato@danielacb.com>
2022-10-05 21:51:47 -0300

    create shouldRenderBanner cypress command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -38,3 +38,22 @@
 //
 // Testing Library Commands
 import '@testing-library/cypress/add-commands'
+
+Cypress.Commands.add('shouldRenderBanner', () => {
+  cy.get('.slick-slider').within(() => {
+    cy.findByRole('heading', { name: /cyberpunk 2077/i })
+    cy.findByRole('link', { name: /buy now/i })
+
+    cy.get('.slick-dots > :nth-child(2) > button').click()
+    cy.wait(500)
+
+    cy.findByRole('heading', { name: /tomb raider: legend/i })
+    cy.findByRole('link', { name: /buy now/i })
+
+    cy.get('.slick-dots > :nth-child(3) > button').click()
+    cy.wait(500)
+
+    cy.findByRole('heading', { name: /sale/i })
+    cy.findByRole('link', { name: /see more info/i })
+  })
+})
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to check banner in page
+     * @example cy.shouldRenderBanner()
+     */
+    shouldRenderBanner(): Chainable<Element>
+  }
+}

a5dded2d8b78282f69d692b2d8eacbb572db438b
Daniela Barbosa <contato@danielacb.com>
2022-10-19 21:03:36 -0300

    add sign in and sign out e2e test to user

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -12,4 +12,17 @@ describe('User', () => {
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username, { timeout: 5000 }).should('exist')
   })
+
+  it('should sign in and sign out', () => {
+    cy.visit('/sign-in')
+    cy.signIn()
+
+    cy.findByText(/e2etest/i)
+      .should('exist')
+      .click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/cypress/i).should('not.exist')
+  })
 })

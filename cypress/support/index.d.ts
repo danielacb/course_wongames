7e38c24401118f63a3e0243cc4119aeabcc5031c
Daniela Barbosa <contato@danielacb.com>
2022-10-24 22:53:01 -0300

    create addToCartByIndex and removeFromCartByIndex cypress commands

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -65,5 +65,17 @@ declare namespace Cypress {
      * @example cy.signUp(user)
      */
     signIn(email?: string, password?: string): Chainable<Element>
+
+    /**
+     * Custom command to add game to cart by index
+     * @example cy.addToCartByIndex(1)
+     */
+    addToCartByIndex(value: number): Chainable<Element>
+
+    /**
+     * Custom command to remove game from cart by index
+     * @example cy.removeFromCartByIndex(2)
+     */
+    removeFromCartByIndex(value: number): Chainable<Element>
   }
 }
